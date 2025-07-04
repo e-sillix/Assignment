199997d82ee2da8cd23c5ca8e3c4381190767bfb
Sumit Panwar <93528833+e-sillix@users.noreply.github.com>
2025-07-04 14:00:55 +0530

    checkpoint

---
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    [SerializeField] private Animator playerAnimator;
+    void Start()
+    {
+
+    }
+    public void TriggerRunning()
+    {
+
+    }
+    public void TriggerTackling()
+    {
+
+    }
+    public void TriggerCollapsing()
+    {
+
+    }
+    public void TriggerWinnning()
+    {
+        
+    }
+}
